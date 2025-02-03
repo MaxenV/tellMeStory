e477abbad41d1bde4d8c65f00389860b46648298
MaxenV <maxen2001@gmail.com>
2025-02-03 15:19:52 +0100

    Create HomePageView

--- src/tellMeStoryBackend/tellMeStory/views.py
+++ src/tellMeStoryBackend/tellMeStory/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.views import View
 
 
+class HomePageView(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "home.html", context)
+
+
 class ChatPageView(View):
 
     def get(self, request, *args, **kwargs):
