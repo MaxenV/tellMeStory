9cebad4039b161cef5a057dce68df5013da70f7b
MaxenV <maxen2001@gmail.com>
2025-03-03 12:42:11 +0100

    Implement Ollama API service methods and update views for model management

--- src/tellMeStoryBackend/api/services/ollama_api_service.py
+++ src/tellMeStoryBackend/api/services/ollama_api_service.py
@@ -1,21 +1,86 @@
+import requests
+from api.dto.ModelListDTO import ModelListDTO
+
+
 class OllamaApiService:
     def __init__(self, base_url: str):
         self.base_url = base_url
+        self.role = "user"
 
     def server_is_present(self):
-        pass
+        try:
+            response = requests.get(self.base_url, json={})
+            if response.text == "Ollama is running":
+                return True
+            else:
+                return False
+        except requests.exceptions.RequestException:
+            return Exception("Connection error")
 
     def list_all_models(self):
-        pass
+        try:
+            response = requests.get(f"{self.base_url}/api/tags")
+            output = ModelListDTO(models=response.json())
+            return list(output)
+        except requests.exceptions.RequestException:
+            return Exception("Connection error")
 
     def list_running_models(self):
-        pass
+        try:
+            response = requests.get(f"{self.base_url}/api/ps")
+            output = ModelListDTO(models=response.json())
+            return list(output)
+        except requests.exceptions.RequestException:
+            return Exception("Connection error")
 
-    def run_model(self):
-        pass
+    def run_model(self, model_name):
+        try:
+            models_list = self.list_all_models()
+            if type(models_list) is Exception:
+                return models_list
 
-    def stop_model(self):
-        pass
+            model_names = list(map(lambda model: model["name"], models_list))
+            if model_name in model_names:
+                response = requests.post(
+                    f"{self.base_url}/api/generate", json={"model": model_name}
+                )
+                return response.json().get("done")
+            else:
+                return Exception("No model with this name")
+        except requests.exceptions.RequestException:
+            return Exception("Connection error")
 
-    def send_request(self):
-        pass
+    def stop_model(self, model_name):
+        try:
+            if model_name in map(
+                lambda model_obj: model_obj["name"], self.list_running_models()
+            ):
+                response = requests.post(
+                    f"{self.base_url}/api/generate",
+                    json={"model": model_name, "keep_alive": 0},
+                )
+                return response.json().get("done")
+            else:
+                return Exception("No model with this name")
+        except requests.exceptions.RequestException:
+            return Exception("Connection error")
+
+    def send_request(self, model_name, content, role=None):
+        try:
+            if role is None:
+                role = self.role
+            response = requests.post(
+                f"{self.base_url}/api/chat",
+                json={
+                    "model": model_name,
+                    "stream": False,
+                    "messages": [{"role": "user", "content": content}],
+                },
+            )
+            response = response.json()
+            if response.get("done") is True:
+                return response
+            else:
+                return Exception("Chat response error")
+        except requests.exceptions.RequestException as e:
+            return Exception("Connection error")
--- src/tellMeStoryBackend/api/views.py
+++ src/tellMeStoryBackend/api/views.py
@@ -1,71 +1,77 @@
 import json
-from datetime import datetime
 
-import requests
 from django.http import JsonResponse
 from django.utils.decorators import method_decorator
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 
-from api.dto.ModelListDTO import ModelListDTO
+from . import ollama_service
 
 
 class PresentView(View):
     def get(self, request, *args, **kwargs):
-        output = "Unknown error"
-        try:
-            response = requests.get("http://localhost:22545", json={})
-            output = response.text
-        except requests.exceptions.RequestException as e:
-            print(e)
+        if ollama_service.server_is_present():
+            output = "Ollama is running"
+        else:
             output = "Ollama server is not running"
+
         return JsonResponse({"present": output})
 
 
 class ModelAllView(View):
     def get(self, request, *args, **kwargs):
-        response = requests.get("http://127.0.0.1:22545/api/tags", json={})
-        output = ModelListDTO(models=response.json())
-        return JsonResponse({"models": list(output)})
+        output = ollama_service.list_all_models()
+        if type(output) is Exception:
+            return JsonResponse({"error": output.args})
+        else:
+            return JsonResponse({"models": output})
 
 
 class ModelRunningView(View):
     def get(self, request, *args, **kwargs):
-        response = requests.get("http://127.0.0.1:22545/api/ps", json={})
-        output = ModelListDTO(models=response.json())
-        return JsonResponse({"models": list(output)})
+        output = ollama_service.list_running_models()
+        if type(output) is Exception:
+            return JsonResponse({"error": output.args})
+        else:
+            return JsonResponse({"models": output})
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 class ModelRunView(View):
     def post(self, request, *args, **kwargs):
         model = json.loads(request.body)["model"]
-        print()
-        print(request.POST)
-        response = requests.post(
-            "http://127.0.0.1:22545/api/generate", json={"model": model}
-        )
-        return JsonResponse({"result": response.json()})
+        output = ollama_service.run_model(model)
+        if type(output) is Exception:
+            return JsonResponse({"error": output.args})
+        else:
+            return JsonResponse({"done": output})
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 class ModelStopView(View):
     def post(self, request, *args, **kwargs):
         model = json.loads(request.body)["model"]
-        print()
-        print(request.POST)
-        response = requests.post(
-            "http://127.0.0.1:22545/api/generate",
-            json={"model": model, "keep_alive": 0},
-        )
-        return JsonResponse({"result": response.json()})
+        output = ollama_service.stop_model(model)
+        if type(output) is Exception:
+            return JsonResponse({"error": output.args})
+        else:
+            return JsonResponse({"done": output})
 
 
 class ResponseView(View):
     def get(self, request, *args, **kwargs):
-        role = request.GET.get("role")
-        content = request.GET.get("content")
-        response = requests.get(
-            "http://127.0.0.1:22545/response", json={"role": role, "content": content}
-        )
-        return JsonResponse({"message": response.json(), "time": datetime.now()})
+        model, content, role = self._get_request_attributes(request)
+        response = ollama_service.send_request(model, content, role)
+        if type(response) is Exception:
+            return JsonResponse({"error": response.args})
+        else:
+            return JsonResponse({"response": response})
+
+    def _get_request_attributes(self, request):
+        loads = json.loads(request.body)
+        if not {"model", "content"}.issubset(loads):
+            return JsonResponse({"error": "Request should contain model and content"})
+        model = loads["model"]
+        content = loads["content"]
+        role = loads["role"] if "role" in loads.keys() else None
+        return model, content, role
